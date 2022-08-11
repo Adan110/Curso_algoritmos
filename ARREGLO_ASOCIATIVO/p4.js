29eeb65bbd5963ea2598649c3fda0549bde221d1
Adan110 <110443916+Adan110@users.noreply.github.com>
2022-08-10 19:05:40 -0500

    Tarea arreglo asociativo

---
+++ ARREGLO_ASOCIATIVO/p4.js
@@ -0,0 +1,32 @@
+var yo = {
+    "Nombre" : "Adan",
+    "Edad" : "15",
+    "Gustos" : {
+        "Musica" : {
+            "Album 1" : "Clasificacion C",
+            "Album 2" : "Baul",
+            "Album 3" : "Cambios"
+        },
+        "Peliculas favoritas" : {
+            "Pelicula 1" : "Operacion dragon",
+            "Pelicula 2" : "Samurai X",
+            "Pelicula 3" : "Ip man"
+        },
+        "Comidas" : {
+            "Comida 1" : "Tacos dorados",
+            "Comida 2" : "Pechuga",
+            "Comida 3" : "Ensalada"
+        },
+        "Animales" :{
+            "Animal 1" : "Perro",
+            "Animal 2" : "Leon",
+            "Animal 3" : "Tigre"
+        },
+        "Videojuegos" : {
+            "Videojuego 1" : "Fifa",
+            "Videojuego 2" : "Palandis",
+            "Videojuego 3" : "Destiny"
+        }
+    }     
+};
+console.log(yo);
